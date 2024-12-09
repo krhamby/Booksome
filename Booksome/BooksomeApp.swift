284a21e000c25da373351546f60181b8d196efeb
Kevin Hamby <82542818+krhamby@users.noreply.github.com>
2024-12-09 17:55:14 -0500

    Initial commit

---
+++ Booksome/BooksomeApp.swift
@@ -0,0 +1,32 @@
+//
+//  BooksomeApp.swift
+//  Booksome
+//
+//  Created by Kevin Hamby on 12/7/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BooksomeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
